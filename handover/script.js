b69ce37922387183e67620a5d4405843dac694d6
Rodrigo Santos <rodrigo.santos@dgt.com.br>
2023-11-06 10:40:00 -0300

    form validator

--- handover/script.js
+++ handover/script.js
@@ -6,6 +6,7 @@ var env = {
         idClientSelect: 'c099f419-19'
     },
     bootstrap: {
+        idFormCalendar: 'idFormCalendar1',
         idCalendarStart: 'dateStart1',
         idCalendarEnd: 'dateEnd1',
         idBtnCalendar: 'idBtnCalendar1',
@@ -33,12 +34,19 @@ var dateTime = {
 
 function init() {
     setStartAndEndDates();
-    callFunc(replicateAndSyncSelects)
+    callFunc(replicateAndSyncSelects);
 }
 
 // Btn Pesquisar chama a função
 function search() {
+    if(!checkFormValid())
+        return false;
+
+    changeDateKlipCalendar(env.bootstrap.idCalendarStart);
+    changeDateKlipCalendar(env.bootstrap.idCalendarEnd);
+
     btnBlockRequest();
+    return false;
 }
 window.search = search;
 
@@ -57,8 +65,8 @@ function btnBlockRequest() {
     
     btnBoots.disabled = true;
     btnBoots.style.cursor = 'wait';
-    btnBoots.innerHTML = `Aguarde ${counter}`;
-    
+    btnBoots.innerHTML = `Aguarde ${counter}s`;
+
     var countdown = setInterval(function() {
         counter--;
         if (counter <= 0) {
@@ -67,14 +75,14 @@ function btnBlockRequest() {
             btnBoots.style.cursor = 'pointer';
             btnBoots.innerHTML = 'Pesquisar';
         } else {
-            btnBoots.innerHTML = `Aguarde ${counter}`;
+            btnBoots.innerHTML = `Aguarde ${counter}s`;
         }
     }, 1000);
 }
 
 function setStartAndEndDates() {
-    let dateStart = document.getElementById('dateStart1');
-    let dateEnd = document.getElementById('dateEnd1');
+    let dateStart = document.getElementById(env.bootstrap.idCalendarStart);
+    let dateEnd = document.getElementById(env.bootstrap.idCalendarEnd);
   
     if (!dateStart || !dateEnd) {
       console.error('Start date or end date input not found.');
@@ -103,16 +111,48 @@ function changeDateKlipCalendar(id) {
     if (!klip || !bootsInput)
         return;
     
-
-    klip = klip.querySelector('input');
-
-    if (!klip)
-        return;
     
+    klip = klip.querySelector('input');
+    
+    if (!klip)
+    return;
 
     klip.value = bootsInput.value;
+    checkDateStartDateEnd();
 }
 window.changeDateKlipCalendar = changeDateKlipCalendar;
+
+function checkFormValid() {
+    let form = document.getElementById(env.bootstrap.idFormCalendar);
+    if (!form)
+        return false;
+    //verificar se form é valido usando metodo form.checkValidity()
+    if (!form.checkValidity())
+        return false;
+
+    return true;
+}
+
+function checkDateStartDateEnd() {
+    let dateStart = document.getElementById(env.bootstrap.idCalendarStart);
+    let dateEnd = document.getElementById(env.bootstrap.idCalendarEnd);
+
+    if (!isValidDate(dateStart.value) || !isValidDate(dateEnd.value))
+        return false;
+    if (dateStart.value > dateEnd.value) {
+        dateStart.setCustomValidity('A data inicial não pode ser superior à data final.');
+        return false
+      } else {
+        dateStart.setCustomValidity('');
+        return true;
+      }
+}
+
+//Verifica se a data é valida
+function isValidDate(dateString) {
+    var date = new Date(dateString);
+    return !isNaN(date.getTime());
+}
 
 //select
 function replicateAndSyncSelects() {
@@ -156,150 +196,4 @@ init();
 
 
 
-debugger;
-
-
-
-
-
-
-
-{
-    "type"= "input",
-    "id"= "0d09e944-42",
-    "displayName"= "DataFim",
-    "layoutConfig"= {
-       "x": 1,
-       "y": 4
-    },
-    "renamed"= true,
-    "fmtArgs"= {
-       "useEOD": true
-    },
-    "dstContext"= {
-       "id": "dst0d09e94442",
-       "kid": "d5e2ca1f0396bc3eb41880711bb1bd3a"
-    },
-    "components"= [
-       {
-          "type": "calendar_display_format",
-          "role": "display_format",
-          "id": "93f62010-45",
-          "displayName": "Display Format",
-          "fmt": "dat2",
-          "fmtArgs": {
-             "dateInputFormat": "dd/MM/yyyy",
-             "dateFormat": "custom",
-             "dateFormatCustom": "dd/MM/yyyy",
-             "dateInputFormatCustom": "dd/MM/yyyy",
-             "resultMetadata": {
-                "ignoreDateFormat": false,
-                "isAggregated": false,
-                "resultAutoDetectedFmt": {
-                   "fmtArgs": {},
-                   "defaultAggregate": "COUNT",
-                   "fmt": "txt"
-                },
-                "formulaIsDataPointer": false,
-                "suggestedName": "Unnamed 1",
-                "isDSTApplied": true,
-                "resultCount": 1
-             }
-          },
-          "formulas": [
-             {
-                "txt": "\"\"",
-                "ver": 2
-             }
-          ],
-          "data": [
-             [
-                ""
-             ]
-          ],
-          "autoFmt": false
-       },
-       {
-          "type": "output_format",
-          "role": "output_format",
-          "id": "d8988e02-46",
-          "displayName": "Output Format",
-          "fmt": "dat2",
-          "fmtArgs": {
-             "dateInputFormat": "MMMM d, yyyy",
-             "dateFormat": "dd/MM/yyyy",
-             "dateFormatCustom": "dd/MM/yyyy",
-             "useEOD": "true",
-             "resultMetadata": {
-                "ignoreDateFormat": false,
-                "isAggregated": false,
-                "resultAutoDetectedFmt": {
-                   "fmtArgs": {},
-                   "defaultAggregate": "COUNT",
-                   "fmt": "txt"
-                },
-                "formulaIsDataPointer": false,
-                "suggestedName": "Unnamed 1",
-                "isDSTApplied": true,
-                "resultCount": 1
-             }
-          },
-          "formulas": [
-             {
-                "txt": "\"\"",
-                "ver": 2
-             }
-          ],
-          "data": [
-             [
-                ""
-             ]
-          ],
-          "autoFmt": false
-       },
-       {
-          "type": "calendar_display_format",
-          "role": "calendar_format",
-          "id": "e4ea1f59-47",
-          "displayName": "Calendar Format",
-          "fmt": "dat2",
-          "fmtArgs": {
-             "dateInputFormat": "dd/MM/yyyy",
-             "dateFormat": "MMMM d, yyyy",
-             "dateFormatCustom": "MMMM d, yyyy",
-             "dateInputFormatCustom": "dd/MM/yyyy",
-             "resultMetadata": {
-                "ignoreDateFormat": false,
-                "isAggregated": false,
-                "resultAutoDetectedFmt": {
-                   "fmtArgs": {},
-                   "defaultAggregate": "COUNT",
-                   "fmt": "txt"
-                },
-                "formulaIsDataPointer": false,
-                "suggestedName": "Unnamed 1",
-                "isDSTApplied": true,
-                "resultCount": 1
-             }
-          },
-          "formulas": [
-             {
-                "txt": "\"\"",
-                "ver": 2
-             }
-          ],
-          "data": [
-             [
-                ""
-             ]
-          ],
-          "autoFmt": false
-       }
-    ],
-    "scope"= 2,
-    "propName"= "handoverDateEnd",
-    "inputType"= 4,
-    "inputLabel"= "Fim",
-    "submitAsGroup"= "1",
-    "useButton"= "18740ca0-39****"
- }
+//debugger;
